f54ad51463cc9c9dab0ad2076ae554ff4189b0e7
Resul <resul9gungorrr@gmail.com>
2026-01-16 21:47:27 +0300

    Terminalde çalışan kodu arayüze geçirme işlemi (tkinter)

--- mayintarlasi.py
+++ mayintarlasi.py
@@ -1,93 +1,97 @@
+import tkinter as tk
+from tkinter import messagebox
 import random
 
-olcek = int(input("Tarla ölçeğini giriniz (örn: 9 için 9x9 tarla): "))
-# tarla oluşturma
-tarla = [[0] * olcek for _ in range(olcek)]
-toplammayin = 0
-for i in range(olcek):
-    for j in range(olcek):
-        if random.random() < 0.20:
-            tarla[i][j] = '*'
-            toplammayin += 1
+class MayinTarlasiGUI:
+    def __init__(self, pencere, olcek=10):
+        self.pencere = pencere
+        self.pencere.title("Mayın Tarlası")
+        self.olcek = olcek
+        self.butonlar = [[None for _ in range(olcek)] for _ in range(olcek)]
+        self.tarla = [[0] * olcek for _ in range(olcek)]
+        self.toplam_mayin = 0
+        
+        self.oyunu_kur()
+        self.arayuzu_olustur()
 
+    def oyunu_kur(self):
+        # Mantık: Mayınları yerleştir ve sayıları hesapla
+        for i in range(self.olcek):
+            for j in range(self.olcek):
+                if random.random() < 0.15: # %15 mayın ihtimali
+                    self.tarla[i][j] = '*'
+                    self.toplam_mayin += 1
+                    # Çevredeki sayıları artır
+                    for di in (-1, 0, 1):
+                        for dj in (-1, 0, 1):
+                            ni, nj = i + di, j + dj
+                            if 0 <= ni < self.olcek and 0 <= nj < self.olcek:
+                                if isinstance(self.tarla[ni][nj], int):
+                                    self.tarla[ni][nj] += 1
+
+    def arayuzu_olustur(self):
+        # Grid (ızgara) yapısıyla butonları oluşturma
+        for r in range(self.olcek):
+            for c in range(self.olcek):
+                btn = tk.Button(self.pencere, text=" ", width=4, height=2, 
+                               command=lambda r=r, c=c: self.hucre_tikla(r, c),
+                               font=('Arial', 10, 'bold'), bg="#CCCCCC")
+                btn.grid(row=r, column=c)
+                self.butonlar[r][c] = btn
+
+    def hucre_tikla(self, r, c):
+        # Eğer hücre zaten açılmışsa işlem yapma
+        if self.butonlar[r][c]["state"] == "disabled":
+            return
+
+        icerik = self.tarla[r][c]
+
+        if icerik == '*':
+            self.butonlar[r][c].config(text="💣", bg="red")
+            self.oyun_bitti(False)
+        else:
+            self.hucre_ac(r, c)
+            if self.kazanma_kontrol():
+                self.oyun_bitti(True)
+
+    def hucre_ac(self, r, c):
+        # Senin 'tarla_acici' fonksiyonunun GUI versiyonu
+        if self.butonlar[r][c]["state"] == "disabled":
+            return
+
+        deger = self.tarla[r][c]
+        self.butonlar[r][c].config(text=deger if deger > 0 else "", 
+                                   state="disabled", relief=tk.SUNKEN, bg="#EEEEEE")
+
+        # Eğer hücre 0 ise komşuları da aç (Rekürsif)
+        if deger == 0:
             for di in (-1, 0, 1):
                 for dj in (-1, 0, 1):
-                    if di == 0 and dj == 0:
-                        continue
-                    ni, nj = i + di, j + dj
-                    if 0 <= ni < olcek and 0 <= nj < olcek:
-                        if isinstance(tarla[ni][nj], int):
-                            tarla[ni][nj] += 1
+                    ni, nj = r + di, c + dj
+                    if 0 <= ni < self.olcek and 0 <= nj < self.olcek:
+                        self.hucre_ac(ni, nj)
 
-tarlaAciklik = [[0] * olcek for _ in range(olcek)]
-def tarla_acici(a,b):
-    if tarlaAciklik[a][b] == 1:
-        return 1
-    tarlaAciklik[a][b] = 1
-    # etrafı açma
-    if tarla[a][b] == 0:
-        for di in (-1, 0, 1):
-            for dj in (-1, 0, 1):
-                if di == 0 and dj == 0:
-                    continue
-                ni, nj = a + di, b + dj
-                if 0 <= ni < olcek and 0 <= nj < olcek:
-                    tarlaAciklik[ni][nj] = 1
-                    if tarla[ni][nj] == 0:
-                        tarla_acici(ni, nj)
-                    #tarla_acici(ni, nj)
+    def kazanma_kontrol(self):
+        # Kapalı olan ve mayın olmayan hücre var mı?
+        for r in range(self.olcek):
+            for c in range(self.olcek):
+                if self.tarla[r][c] != '*' and self.butonlar[r][c]["state"] != "disabled":
+                    return False
+        return True
 
-# ekran kısmı
-def ekran_tarla(tarla, tarlaAciklik):
-    print("      ", end='')
-    for i in range(olcek):
-        print('[' + chr(65 + i) + ']', end=' ')
-    print()
-    print("      ", end='')
-    for i in range(olcek):
-        print(' | ', end=' ')
-    print()
+    def oyun_bitti(self, kazandi):
+        # Tüm mayınları göster
+        for r in range(self.olcek):
+            for c in range(self.olcek):
+                if self.tarla[r][c] == '*':
+                    self.butonlar[r][c].config(text="💣", bg="#FFAAAA")
+                self.butonlar[r][c].config(state="disabled")
 
-    for satir in tarla:
-        if tarla.index(satir) + 1 < 10:
-            print(f"[ {tarla.index(satir)+1}]", end='--')
-        else:
-            print(f"[{tarla.index(satir)+1}]", end='--')
+        mesaj = "Tebrikler, Kazandınız!" if kazandi else "Mayına Bastınız! Oyun Bitti."
+        messagebox.showinfo("Sonuç", mesaj)
+        self.pencere.destroy()
 
-        for hucre in satir:
-            if tarlaAciklik[tarla.index(satir)][satir.index(hucre)] == 1:
-                print(f'[{hucre}]', end=' ')
-            else:
-                print(f'[?]', end=' ')
-        print()
-ekran_tarla(tarla, tarlaAciklik)
-# kullanıcıdan kordinat alma kısmı
-while True:
-    print("\nMayınlı tarlada gezinmek için kordinat giriniz (örn: A5). Çıkmak için 'q' tuşuna basınız.")
-    print(f"Toplam mayın sayısı: {toplammayin}")
-    print(f"Toplam hücre sayısı: {olcek * olcek}")
-    print(f"Açılan hücre sayısı: {sum(sum(1 for hucre in satir if hucre == 1) for satir in tarlaAciklik)}")
-    koordinat = input("Kordinat giriniz (örn: A5): ").upper()
-    if koordinat == 'Q':
-        print("Oyundan çıkılıyor.")
-        break
-    if len(koordinat) < 2 or not koordinat[0].isalpha() or not koordinat[1:].isdigit():
-        print("Geçersiz kordinat formatı. Tekrar deneyin.")
-        continue
-
-    sutun = ord(koordinat[0]) - 65
-    satir = int(koordinat[1:]) - 1
-
-    if not (0 <= sutun < olcek and 0 <= satir < olcek):
-        print("Kordinatlar tarla sınırları dışında. Tekrar deneyin.")
-        continue
-
-    if tarla[satir][sutun] == '*':
-        print("Mayına bastınız! Oyun bitti.")
-        break
-    else:
-        tarla_acici(satir, sutun)
-        ekran_tarla(tarla, tarlaAciklik)
-        print(f"Bu hücre çevresinde {tarla[satir][sutun]} mayın var.")
-        
-    
+# Uygulamayı başlat
+root = tk.Tk()
+uygulama = MayinTarlasiGUI(root, olcek=10)
+root.mainloop()
